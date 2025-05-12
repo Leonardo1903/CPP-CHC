9dd5b2dbbda613df5eb068b2d1a1224f5bc4cb3b
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2025-05-12 14:10:01 +0530

    Variables and Constants Done

---
+++ 01_VarConst/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score;
+    score = 100;
+
+    int balance = 1000;
+    balance = 2000;
+
+    const int uuid = 1234;
+    // uuid = 5678;
+    
+    cout << "Hello, World 1!" << endl;
+    cout << "Hello, World 2!" << endl;
+    cout << "Hello, World 3!" << endl;
+    return 0;
+}
